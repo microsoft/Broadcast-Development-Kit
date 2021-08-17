0c9ee703793abca269e9786f71fb530b7fbf1488
Angel Verdu <ing.angelverdu@gmail.com>
2021-08-17 15:49:28 -0300

    Feature/add slate media injector (#13)
    Co-authored-by: Ange Verdu<angel.verdu@southworks.com>

---
+++ docs/common/customize_bdk_slate_image.md
@@ -0,0 +1,29 @@
+# Customize the Broadcast Development Kit Slate image
+
+## Getting Started 
+When the BDK is joined into a Microsoft Teams call, it starts rendering a "Slate" image on the bot spot in the Microsoft Teams client while no media content injection is active.
+
+By default, the `Slate` image rendered on the Microsoft client is an image provided in the BDK solution, placed on the BDK `BotService` root solution folder. Once the bot is joined into a meeting the following image is displayed on the Microsoft Teams client.
+
+|![BDK default slate image](images/default-slate-image.png)|
+|:--:|
+|*Slate Image displayed by the bot when joins into the meeting.*|
+
+## Customizing the Slate image
+
+The `Slate` image rendered by the bot on the Microsoft Teams client can be customized. You only need to change the default image provided in the solution with one of your choosing.
+
+To do so, please go to the `BotService` solution folder, find the `PNG` file called `slate.png` and replace it with an image of your choosing. 
+
+|![BDK default slate image route](images/default-slate-image-solution-route.png)|
+|:--:|
+|*Slate image provided on the `BotService` solution*|
+
+The `Slate` image you need to provide must have the following characteristics:
+
+- ***Format***: The image format must be PNG.
+- ***Name***: The image name must be named `slate`. 
+- ***Width***: The image width must be 1920px.
+- ***Height***: The image height must be 1080px.  
+
+> **NOTE**: The image provided in the solution is copied to the folder where the `BotService` is published. You can also change the image directly from the folder where the bot was published, you do not need to build and publish again the solution, every time you want to change the `Slate` image. 
---
+++ src/BotService/Infrastructure/Core/SlateMediaInjector.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System;
+using BotService.Application.Core;
+using BotService.Infrastructure.Common.Logging;
+using BotService.Infrastructure.Pipelines;
+using Gst;
+using Gst.App;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph.Communications.Calls.Media;
+using Microsoft.Skype.Bots.Media;
+
+namespace BotService.Infrastructure.Core
+{
+    public class SlateMediaInjector : IMediaInjector, IDisposable
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger<SlateMediaInjector> _logger;
+        private readonly PipelineBusObserver _pipelineBusObserver;
+
+        private IDisposable _unsubscribeObserver;
+        private IMediaInjectionPipeline _pipeline;
+
+        public SlateMediaInjector(IVideoSocket videoSocket, ILoggerFactory loggerFactory, PipelineBusObserver pipelineBusObserver)
+        {
+            VideoSocket = videoSocket;
+            _loggerFactory = loggerFactory;
+            _pipelineBusObserver = pipelineBusObserver;
+            _logger = _loggerFactory.CreateLogger<SlateMediaInjector>();
+        }
+
+        public IVideoSocket VideoSocket { get; protected set; }
+
+        public void Start(MediaInjectionSettings injectionSettings)
+        {
+            _pipeline = new SlateMediaInjectionPipeline(injectionSettings, _loggerFactory);
+            _pipeline.SetNewVideoSampleHandler(NewVideoSample);
+            _unsubscribeObserver = _pipeline.Subscribe(_pipelineBusObserver);
+            _pipeline.Play();
+        }
+
+        public void Stop()
+        {
+            _pipeline.Stop();
+            _pipeline.RemoveNewVideoSampleHandler(NewVideoSample);
+            _unsubscribeObserver?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _pipeline = null;
+            }
+        }
+
+        private void NewVideoSample(object sender, GLib.SignalArgs args)
+        {
+            var sink = (AppSink)sender;
+            var sample = sink.PullSample();
+
+            if (sample != null)
+            {
+                var buffer = sample.Buffer;
+                buffer.Map(out MapInfo info, MapFlags.Read);
+                var size = (uint)info.Size;
+                var data = info.Data;
+                var timestamp = MediaPlatform.GetCurrentTimestamp();
+                var videoSendBuffer = new VideoSendBuffer(data, size, VideoFormat.NV12_1920x1080_1_875Fps, timestamp);
+
+                try
+                {
+                    VideoSocket.Send(videoSendBuffer);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "[Slate Injection] Video - Error sending buffer - Message: {message}", ex.InnerException?.Message ?? ex.Message);
+                }
+
+                buffer.Unmap(info);
+                buffer.Dispose();
+                sample.Dispose();
+            }
+            else
+            {
+                _logger.LogWarning("[Slate Injection] Video - New sample signal was triggered without a sample.");
+            }
+        }
+    }
+}
